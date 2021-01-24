f19572893ec0cfca5d8701e67da0f6a23e200b53
Alexsander Hendra Wijaya <alexistdev@gmail.com>
2021-01-24 09:18:10 +0700

    First Upload

---
+++ app/src/main/java/com/berkatfaatulohalawa1711010164/facevoting/MainActivity.java
@@ -0,0 +1,14 @@
+package com.berkatfaatulohalawa1711010164.facevoting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
