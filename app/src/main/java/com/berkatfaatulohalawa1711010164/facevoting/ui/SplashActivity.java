1c01a21a1908da4319645f597862882642b82ea5
Alexsander Hendra Wijaya <alexistdev@gmail.com>
2021-01-24 10:43:50 +0700

    Menambahkan design template halaman splash dan prelogin1

--- app/src/main/java/com/berkatfaatulohalawa1711010164/facevoting/ui/SplashActivity.java
+++ app/src/main/java/com/berkatfaatulohalawa1711010164/facevoting/ui/SplashActivity.java
@@ -2,8 +2,12 @@ package com.berkatfaatulohalawa1711010164.facevoting.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
+import com.berkatfaatulohalawa1711010164.facevoting.MainActivity;
 import com.berkatfaatulohalawa1711010164.facevoting.R;
 
 public class SplashActivity extends AppCompatActivity {
@@ -12,5 +16,15 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        TextView btnStart = findViewById(R.id.tombol_start);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SplashActivity.this, preLogin1.class);
+                startActivity(intent);
+            }
+        });
     }
 }
