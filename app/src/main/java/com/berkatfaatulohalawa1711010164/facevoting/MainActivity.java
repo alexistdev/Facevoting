57e03ab4c7ab162c9398431776f052e29ddeb660
Alexsander Hendra Wijaya <alexistdev@gmail.com>
2021-01-26 13:12:55 +0700

    menambahkan halaman dashboard fragment

--- app/src/main/java/com/berkatfaatulohalawa1711010164/facevoting/MainActivity.java
+++ app/src/main/java/com/berkatfaatulohalawa1711010164/facevoting/MainActivity.java
@@ -1,14 +1,55 @@
 package com.berkatfaatulohalawa1711010164.facevoting;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import com.berkatfaatulohalawa1711010164.facevoting.fragment.akun_fragment;
+import com.berkatfaatulohalawa1711010164.facevoting.fragment.hasil_fragment;
+import com.berkatfaatulohalawa1711010164.facevoting.fragment.home_fragment;
+import com.berkatfaatulohalawa1711010164.facevoting.fragment.votefragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loadFragment(new home_fragment());
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomMenu);
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            Fragment fragment;
+            switch (item.getItemId()){
+                case R.id.home_menu:
+                    fragment = new home_fragment();
+                    break;
+                case R.id.vote_menu:
+                    fragment = new votefragment();
+                    break;
+                case R.id.hasil_menu:
+                    fragment = new hasil_fragment();
+                    break;
+                default:
+                    fragment = new akun_fragment();
+                    break;
+            }
+            return loadFragment(fragment);
+        });
+    }
+
+    /* Meload Fragment */
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fl_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
     }
 }
--- app/src/main/java/com/berkatfaatulohalawa1711010164/facevoting/ui/Daftar.java
+++ app/src/main/java/com/berkatfaatulohalawa1711010164/facevoting/ui/Daftar.java
@@ -5,22 +5,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.berkatfaatulohalawa1711010164.facevoting.R;
 
 public class Daftar extends AppCompatActivity {
     private TextView btnLogin;
+    private ImageView btnDaftar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daftar);
 
         btnLogin = findViewById(R.id.tbl_login);
+        btnDaftar = findViewById(R.id.tombol_daftar);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Daftar.this, Login.class);
+                startActivity(intent);
+            }
+        });
+        btnDaftar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Daftar.this, Checkpoint.class);
                 startActivity(intent);
             }
         });
